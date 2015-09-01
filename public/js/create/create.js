e0ff6698eddf99a975176245bd6c90a54d32b7ab
Justin Barabander <jbarabander@gmail.com>
2015-09-01 17:24:27 -0400

    Implemented Create

--- public/js/create/create.js
+++ public/js/create/create.js
@@ -4,7 +4,15 @@ app.config(function($stateProvider) {
 	$stateProvider.state('create', {
 		url: '/create/:userId',
 		templateUrl: 'js/create/create.html',
-		controller: 'CreateCtrl'
+		controller: 'CreateCtrl',
+		resolve: {
+			author: function(User, $stateParams) {
+				return User.find($stateParams.userId)
+				.then(function(result) {
+					return result;
+				})
+			}
+		}
 		/*
 				add a resolve block that has an author function which
 				users $stateParams to retrieve the author object
@@ -13,12 +21,25 @@ app.config(function($stateProvider) {
 })
 
 // add necessary dependencies here
-app.controller('CreateCtrl', function($scope) {
+app.controller('CreateCtrl', function($scope, author, Post, $stateParams, $state) {
+	console.log(author.username);
+	$scope.newPost = {
+		title: null,
+		body: null,
+		name: author.username
+	};
 
 	$scope.previewTrue = false;
 
 	$scope.preview = function() {
 		$scope.previewTrue = !$scope.previewTrue;
+	}
+	$scope.createNewPost = function() {
+		console.log($scope.newPost);
+		Post.create({title: $scope.newPost.title, body: $scope.newPost.body, author: author._id})
+		.then(function(element) {
+			$state.go('main');
+		});
 	}
 
 	/*
